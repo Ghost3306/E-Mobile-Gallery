74d553a7d4cf37a54f884884df04f3855f3c9efa
Lalit Rawool <132126769+Ghost3306@users.noreply.github.com>
2024-05-20 22:41:42 +0530

    Login and Register Design and Input done

---
+++ accounts/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from accounts import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns = [
+    path('',views.home_index,name='homepage'),
+    path('login/',views.login,name='user_login'),
+    path('register-user/',views.register,name='user_register')
+  
+]
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+urlpatterns+=staticfiles_urlpatterns()
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from django.contrib import messages
 # Create your views here.
+def home_index(request):
+    return render(request,'home/homepage.html')
+
+def login(request):
+    if request.method=='POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        print(username,password)
+    # messages.success(request, "Login Success Test")
+    return render(request,'accounts/login.html')
+
+def register(request):
+    if request.method=='POST':
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        confpassword = request.POST.get("confpassword")
+        print(first_name,last_name,email,password,confpassword)
+        if password!=confpassword:
+            messages.warning(request, "Password and Confirm Password doesn't match!")
+    return render(request,'accounts/register.html')
